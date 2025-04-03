18668e757bcf3944776763ca1391c8dd2c5ee1ab
Abishek1-maker <kesu.kunwar@gmail.com>
2025-04-04 02:25:23 +0545

    API2

--- method.js
+++ method.js
@@ -8,6 +8,30 @@ app.get('/api/users',(req,res)=>{
     users
     });
 })
+app.get('/users',(req,res)=>{
+    const html=`
+    <ul>
+    ${users.map((users)=>`<li>${users.first_name}</li>`).join('')}
+</ul>
+    `;
+    res.send(html);
+})
+// app.get('/api/users/:id',(req,res)=>{
+//     const id=Number(req.params.id);
+//     const user=users.find(user=>user.id===id)
+//     res.json(user)
+// })
+
+
+//joining them by route
+app.route('/api/users/:id').get((req,res)=>{
+    const id=Number(req.params.id);
+    const user=users.find(user=>user.id===id)
+    res.json(user);
+}).put((req,res)=>{
+    
+})
+
 app.listen(port,()=>{
     console.log("server is running on 3000");
 })
